4a55ffe0c67bb16df1628f3f882e2b2e7e5dec7f
oliver <braun.o@phystec.de>
2016-10-27 16:01:01 +0200

    changes due to only rst in pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 import os
 import sys
@@ -10,6 +11,12 @@ import unittest
 
 name = "nucosMQ"
 
+#action should be one of update/minor/major
+possible_action = ["major","minor","update", "no-update"]
+action = "update"
+
+if not sys.argv[1] == "sdist":
+    action = "no-update"    
 
 def my_test_suite():
     test_loader = unittest.TestLoader()
@@ -19,7 +26,15 @@ def my_test_suite():
 rootdir = os.path.abspath(os.path.dirname(__file__))
 
 # Restructured text project description read from file
-long_description = open(os.path.join(rootdir, 'README.md')).read()
+# Restructured text project description read from file
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+long_description = read_md('README.md')     #open(os.path.join(rootdir, 'README.md')).read()
 
 # Python 2.7 or later needed
 if sys.version_info < (2, 7, 0, 'final', 0):
@@ -38,6 +53,22 @@ package_data = {} #{name: [os.path.join(name, 'tests', 'prt.txt')]}
 
 # The current version number - MSI accepts only version X.X.X
 exec(open(os.path.join(name, 'version.py')).read())
+
+#update the version count according to action
+if action not in possible_action:
+    raise SystemExit("action should be one of minor/major/update/hand")
+
+if not action == "no-update":
+    version_i = [int(x) for x in version.split(".")]
+    version_i[possible_action.index(action)] += 1
+    version = ".".join([str(x) for x in version_i])
+
+    
+
+    with open(os.path.join(name, 'version.py'), 'w') as f:
+        f.write("version='"+version+"'")
+
+print("Version:", version)
 
 # Scripts
 scripts = []
